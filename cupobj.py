4afecc0c5c9c60a10270fa53dbaeec42f35d1af4
weibk <180515648@qq.com>
2021-10-12 09:16:15 +0800

    Cup类

--- cupobj.py
+++ cupobj.py
@@ -3,7 +3,7 @@
 # author:weibk
 # @time:2021/10/11 19:18
 
-class Cup(object):
+class Cup:
     def __init__(self, height, material, capacity, color):
         self.__height = height
         self.__material = material
@@ -13,6 +13,7 @@ class Cup(object):
     def description(self):
         print(f"这个{self.__color}的杯子高{self.__height}厘米,"
               f"它是用{self.__material}做的，它可以装{self.__capacity}升的水")
+
 
 cup1 = Cup(30, '玻璃', 1.5, '白色')
 cup1.description()
